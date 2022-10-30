1f6ac1a8a00692372dd975a3561e1901d01fc435
mshirdel <mshirdel@gmail.com>
2022-10-30 22:35:05 +0330

    Add swagger

--- feedze/settings/base.py
+++ feedze/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_yasg",
     "feed",
     "user",
 ]
--- feedze/urls.py
+++ feedze/urls.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
 from feed.routers import urls as feed_url
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="FeedzE API",
+        default_version="1.0.0",
+        description="API documentation of Feed",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("user.urls")),
     path("api/v1/", include(feed_url)),
+    path(
+        "docs/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-schema"
+    ),
 ]
