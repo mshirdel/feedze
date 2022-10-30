acb3eb62dbbef973d1eb481c1c4cbe5cfbe0c909
mshirdel <mshirdel@gmail.com>
2022-10-30 14:31:45 +0330

    Add custom user model

--- feedze/settings/base.py
+++ feedze/settings/base.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "feed",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -59,18 +60,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "feedze.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -112,3 +101,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
