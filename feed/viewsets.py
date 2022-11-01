249285c4a49885d30d746dc540f77d1945a45e0e
mshirdel <mshirdel@gmail.com>
2022-11-01 16:55:35 +0330

    Add apis for favorites

--- feed/viewsets.py
+++ feed/viewsets.py
@@ -33,7 +33,7 @@ class FeedViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewS
         instance.unfollow_by_user(request.user.id)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    @action(methods=["get"], detail=False, url_path="user-following")
+    @action(methods=["get"], detail=False, url_path="following")
     def user_following(self, request, *args, **kwargs):
         self.queryset = self.queryset.filter(followers=request.user)
         return self.list(request, *args, **kwargs)
@@ -44,3 +44,29 @@ class FeedItemViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericV
     queryset = model.objects.all()
     serializer_class = FeedItemSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.action == "list":
+            self.queryset = self.queryset.filter(
+                feed__users_follow__user_id=self.request.user.id
+            )
+        return super().get_queryset()
+
+    @action(methods=["post"], detail=True)
+    def favorite(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.favorite_by_user_id(request.user.id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @favorite.mapping.delete
+    def delete_favorite(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.remove_favorite_by_user_id(request.user.id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=["get"], detail=False, url_path="my-favorites")
+    def my_favorites(self, request, *args, **kwargs):
+        self.queryset = self.queryset.filter(
+            users_favorite__user_id=request.user.id
+        ).prefetch_related("users_favorite")
+        return self.list(request, *args, **kwargs)
